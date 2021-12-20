914af298d8f9374e7f4dcc4a8823efc0e55a0986
Jade <jade.cmail@gmail.com>
2021-12-20 10:02:20 +0100

    finished the maven quest1

---
+++ src/main/java/ja/de/App.java
@@ -0,0 +1,12 @@
+package ja.de;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main( String[] args ) {
+        String htmlText = "<h1>Hello World!</h1>";
+        System.out.println( htmlText );
+    }
+}
